eae456a7724b46cc0e6f505b90888c150f0edd59
William Lim <wlim@acromedia.com>
2022-01-04 19:09:42 -0800

    Add color palettes

---
+++ components/themeConfig.js
@@ -0,0 +1,18 @@
+/**
+ * Color palettes for site
+ * 
+ * https://visme.co/blog/website-color-schemes
+ */
+export const palette = {
+  primary: {
+    light: '#FFFFFF',
+    main: '#D3E3FC',
+    dark: '#77A6F7',
+  },
+  pink: {
+    main: '#FFCCBC',
+  },
+  green: {
+    main: '#00887A',
+  }
+}
---
+++ pages/footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      Footer Page
+    </div>
+  )
+}
+
+export default Footer
